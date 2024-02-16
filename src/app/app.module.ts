8c13774b3418cd2af9408c731f8909901331d9b2
vitinhob <vitinhobordin@gmail.com>
2024-02-16 13:49:20 -0300

    projet blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuUserComponent } from './components/menu-user/menu-user.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuUserComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
